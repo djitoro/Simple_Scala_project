13bdd7c2971f95c862971197e1945591829b244e
Дмитрий <160332938+djitoro@users.noreply.github.com>
2025-02-24 05:10:18 +0300

    Update counter_doc.scala
    added partitioning

--- src/main/scala/dimai/engineering/counter_doc.scala
+++ src/main/scala/dimai/engineering/counter_doc.scala
@@ -6,19 +6,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object counter_doc {
   def buildTable(spark: SparkSession, df: DataFrame): DataFrame = {
 
-    // Преобразуем строку времени в Timestamp
-    val formattedData = df.withColumn(
-      "opening_timestamp",
-      to_timestamp(col("date"), "dd.MM.yyyy_HH:mm:ss"))
-
-    // Извлекаем дату из Timestamp
-    val dataWithDate = formattedData.withColumn(
-      "opening_date",
-      to_date(col("opening_timestamp"))
-    )
-
-    // Группируем по названию документа и дате, затем считаем количество открытий
-    val result = dataWithDate
+    val result = df
       .groupBy(col("document_id"), col("opening_date"))
       .agg(count("*").as("openings_count"))
 
